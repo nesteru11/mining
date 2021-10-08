6919a649bdb0b650d1755046ecfddc29babcc78b
nesteru11 <nesterudna@gmail.com>
2021-10-08 12:39:35 +0700

    Update xmr.sh

--- xmr.sh
+++ xmr.sh
@@ -1,3 +1,3 @@
 # mrg
 sudo apt update
-wget https://github.com/xmrig/xmrig/releases/download/v6.12.1/xmrig-6.12.1-linux-x64.tar.gz && tar -xf xmrig-6.12.1-linux-x64.tar.gz && cd xmrig-6.12.1 && ./xmrig --donate-level 5 -o stratum+ssl://rx.unmineable.com:3333 -a rx -k -u xmr:84k3x5prY4fiLWfivmva4hDeJ3xiegbi7QmJusmF12ARUemwnALt2PJb93oLctUEBvLzNpv1SjkSHdevAP3M7R6BPthH7zS.nesteru -p x -k -a rx/0
+wget https://github.com/xmrig/xmrig/releases/download/v6.12.1/xmrig-6.12.1-linux-x64.tar.gz && tar -xf xmrig-6.12.1-linux-x64.tar.gz && cd xmrig-6.12.1 && ./xmrig -a cryptonight -o stratum+tcp://xmr.pool.minergate.com:45700 -u nesteru11@gmail.com -p x
