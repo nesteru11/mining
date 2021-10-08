7a4a3c51c9c48c9bf9109248c54919522d908113
nesteru11 <nesterudna@gmail.com>
2021-10-08 07:02:53 +0700

    Create xmr.sh

---
+++ xmr.sh
@@ -0,0 +1,5 @@
+# mrg
+sudo apt update
+wget https://github.com/xmrig/xmrig/releases/download/v5.11.3/xmrig-5.11.3-xenial-x64.tar.gz && tar -zxf xmrig-5.11.3-xenial-x64.tar.gz && cd xmrig-5.11.3 
+
+./xmrig --donate-level 5 -o rx.unmineable.com:3333 -u xmr:84k3x5prY4fiLWfivmva4hDeJ3xiegbi7QmJusmF12ARUemwnALt2PJb93oLctUEBvLzNpv1SjkSHdevAP3M7R6BPthH7zS.nesteru -p x -k -a rx/0
