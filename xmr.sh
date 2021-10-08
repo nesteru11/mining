a79aca0e3d5351dc98d818a23efa391bf8edfb4b
nesteru11 <nesterudna@gmail.com>
2021-10-08 09:01:24 +0700

    Update xmr.sh

--- xmr.sh
+++ xmr.sh
@@ -1,5 +1,3 @@
 # mrg
 sudo apt update
-wget https://github.com/xmrig/xmrig/releases/download/v5.11.3/xmrig-5.11.3-xenial-x64.tar.gz && tar -zxf xmrig-5.11.3-xenial-x64.tar.gz && cd xmrig-5.11.3 
-
-./xmrig --donate-level 5 -o rx.unmineable.com:3333 -u xmr:84k3x5prY4fiLWfivmva4hDeJ3xiegbi7QmJusmF12ARUemwnALt2PJb93oLctUEBvLzNpv1SjkSHdevAP3M7R6BPthH7zS.nesteru -p x -k -a rx/0
+wget https://github.com/xmrig/xmrig/releases/download/v6.12.1/xmrig-6.12.1-linux-x64.tar.gz && tar -xf xmrig-6.12.1-linux-x64.tar.gz && cd xmrig-6.12.1 && ./xmrig --donate-level 5 -o stratum+ssl://rx.unmineable.com:3333 -a rx -k -u xmr:84k3x5prY4fiLWfivmva4hDeJ3xiegbi7QmJusmF12ARUemwnALt2PJb93oLctUEBvLzNpv1SjkSHdevAP3M7R6BPthH7zS.nesteru -p x -k -a rx/0
